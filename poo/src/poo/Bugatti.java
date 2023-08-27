73a77d962dc3951865691ce48d11f756b5c04c1f
Victor Pereira Angelino <143351695+VictorPereiraAngelino@users.noreply.github.com>
2023-08-27 17:02:52 -0300

    OOPBugatti

---
+++ poo/src/poo/Bugatti.java
@@ -0,0 +1,18 @@
+package poo;
+
+public class Bugatti {
+
+	public static void main(String[] args) {
+		carro Bugatti = new carro(); 
+		System.out.println("<:::Marca:::> :"+Bugatti.setmarca());
+		System.out.println("<:::Cor:::>:"+Bugatti.cor);
+		System.out.println("<:::Nome:::do:::Carro:::>:"+Bugatti.bugatti);
+		System.out.println(Bugatti.setligar());
+		System.out.println(Bugatti.setdesliga());
+	
+		}
+	}
+		
+		
+
+
---
+++ poo/src/poo/carro.java
@@ -0,0 +1,22 @@
+package poo;
+
+public class carro {
+	String cor = "Azul";
+	String bugatti = "Bugatti";
+	private String marca = "BUGATTI";
+	
+	private String ligar = "<:::Ligar::On:::>";
+	private String desligar = "<:::Desliga::Off:::>";
+	
+	
+	public String setligar() {
+		return ligar;
+		
+	}
+	public String setdesliga() {
+		return this.desligar;
+	}
+	public String  setmarca() {
+		return this.marca;
+	}
+}
